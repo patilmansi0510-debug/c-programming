d6dd1f95ce185e677d85c5ba3f1715f0687ed7c9
patilmansi0510-debug <patilmansi0510@gmail.com>
2026-07-17 10:57:12 +0530

    Add files via upload

---
+++ arithmatic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"enter two numbers:";
+cin>>a>>b;
+cout<<"addition"<<a+b<<endl;
+cout<<"subtraction"<<a-b<<endl;
+cout<<"multiplication"<<a*b<<endl;
+cout<<"division"<<a/b<<endl;
+cout<<"modules"<<a%b<<endl;
+}
