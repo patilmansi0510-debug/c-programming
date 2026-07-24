bfa7740efd0cf9f7a18b2548a6e73fc2f08f54ae
patilmansi0510-debug <patilmansi0510@gmail.com>
2026-07-24 10:59:55 +0530

    Add files via upload

---
+++ rec.cpp.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float length, width, area,
+perimeter;
+cout<<"enter length:";
+cin>>length;
+cout<<"enter width:";
+cin>>width;
+area=length*width;
+perimeter=2*(length+width);
+cout<<"area of rectangle="<<area<<endl;
+cout<<"perimeter of rectangle="<<perimeter<<endl;
+return 0;
+}
