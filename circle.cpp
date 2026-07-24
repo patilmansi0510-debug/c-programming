3e727f42875b64e9d69db3ff1d9bfbc4698d479a
patilmansi0510-debug <patilmansi0510@gmail.com>
2026-07-24 11:00:25 +0530

    Add files via upload

---
+++ circle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float radius,area,circumference;
+cout<<"enter the radius of the circle:";
+cin>>radius;
+area=3.14*radius*radius;
+circumference=2*3.14*radius;
+cout<<"area of the circle="<< area << endl;
+cout<<"circumference of the circle="<< circumference << endl;
+return 0;
+}
