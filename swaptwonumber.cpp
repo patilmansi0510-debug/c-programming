2895b2ddc155ed218e0b99098a91be4296c2b97b
patilmansi0510-debug <patilmansi0510@gmail.com>
2026-07-24 11:00:59 +0530

    Add files via upload

---
+++ swaptwonumber.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,temp;
+cout<<"enter first number:";
+cin>>a;
+cout<<"enter second number:";
+cin>>b;
+temp=a;
+a=b;
+cout<<"after swapping:"<< endl;
+cout<<"first number ="<< a << endl;
+cout<<"second number ="<< b << endl;
+return 0;
+}
+
